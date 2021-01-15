fdf23b769318502dec31d1972119c9340af1ce19
colmProneBoxever <75018672+colmProneBoxever@users.noreply.github.com>
2021-01-15 09:49:39 +0000

    Update popup.js

--- Web Experiences/Blueprints/popup-takeover/popup.js
+++ Web Experiences/Blueprints/popup-takeover/popup.js
@@ -1,7 +1,7 @@
-insertHTMLAfter("body");
-const bxModal = document.getElementById("bx-modal_overlay");
-bxModal.style.display = "flex";
-const bxModalClose = bxModal.querySelector(".bx-modal__btn-close-icon");
-bxModalClose.onclick = function() {
-    bxModal.style.display = "none";
+insertHTMLBefore("body");
+document.querySelector("#bx-modal_overlay").style.display = "flex";
+document.body.classList.add("bx-modal_content");
+var bxCardClose = document.querySelector(".bx-modal__btn-close-icon");
+bxCardClose.onclick = function() {
+    document.querySelector("#bx-modal_overlay").style.display = "none";
 }
