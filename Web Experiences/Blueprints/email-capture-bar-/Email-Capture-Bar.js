3dc146cb1211cc706031fca3702a9f77357596eb
Samuel270 <Sam.Pezzaioli@boxever.com>
2021-04-16 09:11:57 +0200

    first 3 experiences

---
+++ Web Experiences/Blueprints/email-capture-bar-/Email-Capture-Bar.js
@@ -0,0 +1,93 @@
+//make experience unique
+var compiledCSS = Boxever.templating.compile(variant.assets.css)(variant);
+var styleTag = document.getElementById('style-' + variant.ref);
+if (styleTag) {
+    styleTag.innerHTML = compiledCSS;
+}
+////
+
+insertHTMLBefore("body");
+
+// start with hidden banner (show on scroll)
+let banner = document.querySelector("#bx_TopBanner");
+banner.style.display = "none";
+
+// declarations
+const bxButtonPress = document.getElementById('bx_TopBanner-button');
+const bxCloseButtonPress = document.querySelector(".bx__btn-close__icon");
+let isBannerBeenClosed = false;
+
+//trigger experience
+const scrollPercentageInput = [[ Show at percentage page | enum(0,25,50,100)| 0 |{group: 0 General}]];
+
+
+// Listeners
+// when scrolling the experience will trigger
+window.onscroll = ()=>{ currentScrollPercentage()}
+
+// when sending email, verify ? sendDataToBoxever() or "stop event" ;
+bxButtonPress.onclick = ()=>{
+    isBannerBeenClosed = true;
+    let emailVerified = validateEmail();
+    if(emailVerified){
+        hideBar();
+        sendDataToBoxever("INTERACTION_CLICKED")
+        showThankYou();
+    }else{
+        //friendly error
+        document.getElementById("bx-email_input").style.backgroundColor = 'rgba(200,0,0,0.1)'
+        isBannerBeenClosed = false;
+    }
+};
+
+//dismiss bar;
+bxCloseButtonPress.onclick = () =>{
+    isBannerBeenClosed = true;
+    hideBar();
+   sendDataToBoxever("INTERACTION_DISMISSED")
+};
+
+// functions
+const currentScrollPercentage = ()=> {
+    const scrollPercentage = Math.round((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100);
+
+    if (scrollPercentage > scrollPercentageInput && !isBannerBeenClosed){
+        document.querySelector(`#bx-${variant.ref} #bx_TopBanner`).style.display = "flex";
+        document.body.classList.add("show-TopBanner");
+    }
+}
+
+const showThankYou= ()=>{
+    let thanksMessage = document.querySelector('#bx-thank_you_modal');
+    thanksMessage.style.display = "block";
+
+    setTimeout(()=>{ thanksMessage.style.display= 'none'; }, 1500);
+}
+
+// dismiss bar
+const hideBar = ()=>{
+    document.querySelector("#bx_TopBanner").style.display = "none";
+    document.body.classList.remove("show-TopBanner");
+}
+
+const sendDataToBoxever = (eventType)=>{
+    let eventToSent = {
+        "channel": "WEB",
+        "type": eventType,
+        "pos": window._boxever_settings.pointOfSale,
+        "browser_id": Boxever.getID(),
+        "interactionID":"OOB_EXP",
+	    "interactionName": "ALERT_BAR_SCROLL"
+    };
+    Boxever.eventCreate(eventToSent, (data)=>{ }, 'json');
+}
+
+// validate text if Mail format
+const validateEmail = () =>{
+    let bxEmail = document.getElementById("bx-email_input").value;
+    let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(bxEmail)
+    let validation = false;
+    mailformat ? validation = true: validation = false;
+    return validation;
+}
+
--- Web Experiences/Blueprints/email-capture-corner/email-capture-corner.js
+++ Web Experiences/Blueprints/email-capture-corner/email-capture-corner.js
@@ -1,18 +1,45 @@
+//make experience unique
+var compiledCSS = Boxever.templating.compile(variant.assets.css)(variant);
+var styleTag = document.getElementById('style-' + variant.ref);
+if (styleTag) {
+    styleTag.innerHTML = compiledCSS;
+}
+/////
+
 insertHTMLAfter("body");
-var bxContent = document.querySelector("#bx-transition-card");
+
+// show popup on bx load
+let bxContent = document.querySelector("#bx-transition-card");
 bxContent.style.display = "flex";
-setTimeout(function(){
-    bxContent.classList.add("open");
-}, 10);
 
-
+//declarations
 const bxEmailCaptureContainer = document.getElementById("bx-email_capture_container")
 const bxThankYouContainer = document.getElementById("bx-thank_you_container")
 const bxClose = bxContent.querySelector(".bx__btn-close__icon");
+const bxCTA = document.getElementById('bx-transition-card--primary');
 
-const bxButtonPress = document.getElementById('bx-transition-card--primary');
-function sendDataToBoxever(eventType) {
-    var eventToSend = {
+
+// LIsteners
+//on Email submission
+bxCTA.onclick = ()=>{
+    let bxEmail = document.getElementById("bx-email_input").value;
+    let emailVerified = validateEmail(bxEmail);
+    emailVerified ?
+        onSuccessValidation(bxEmail)
+    :
+        //friendly error
+        document.getElementById("bx-email_input").style.backgroundColor = 'rgba(200,0,0,0.1)';
+};
+
+bxClose.onclick = ()=>{
+    bxContent.style.display = "none";
+    sendDataToBoxever("INTERACTION_DISMISSED")
+}
+
+
+//declare functions
+const sendDataToBoxever = (eventType)=>{
+    let eventToSend = {
         "channel": "WEB",
         "type": eventType,
         "pos": window._boxever_settings.pointOfSale,
@@ -20,48 +47,36 @@ function sendDataToBoxever(eventType) {
         "interactionID":"OOB_EXP",
 	    "interactionName": "CORNER_POPUP_EMAIL"
     };
-    Boxever.eventCreate(eventToSend, function(data) {}, 'json');
+    Boxever.eventCreate(eventToSend, (data)=>{}, 'json');
 }
 
-bxButtonPress.onclick = function() {
-    let bxEmail = document.getElementById("bx-email_input").value;
-
-    //declare function 
-    const onSuccessValidation = (email)=>{
-        sendDataToBoxever("INTERACTION_IDENTITY")
-        var event = {
-            "channel": "WEB",
-            "type": "IDENTITY",
-            "language": "EN",
-            "currency": "EUR",
-            "page": "Home",
-            "pos": "spinair.com",
-            "browser_id": Boxever.getID(),
-            "email":email
-        };
-        Boxever.eventCreate(event, function(data) {}, 'json');
-        
-        bxEmailCaptureContainer.style.display = "none";
-        bxThankYouContainer.style.display = "flex";
-        setTimeout(function(){ document.querySelector('#bx-transition-card').style.display= 'none'; }, 1500);
-    }
-
-    // validate text if Mail format
-    const ValidateEmail = (bxEmail) =>{
-        let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(bxEmail)
-        if(mailformat){
-            onSuccessValidation(bxEmail)
-            return true;
-        }else{
-            //friendly error
-            document.getElementById("bx-email_input").style.backgroundColor = 'rgba(200,0,0,0.1)'
-            return false;
-        }
-    }
-    ValidateEmail(bxEmail);
-        
-};
-
-bxClose.onclick = function() {
-    bxContent.style.display = "none";
+const onSuccessValidation = (email)=>{
+    sendDataToBoxever("INTERACTION_IDENTITY")
+    let event = {
+        "channel": "WEB",
+        "type": "IDENTITY",
+        "language": "EN",
+        "currency": "EUR",
+        "page": "Home",
+        "pos": "spinair.com",
+        "browser_id": Boxever.getID(),
+        "email":email
+    };
+    Boxever.eventCreate(event, (data)=>{}, 'json');
+    
+    bxEmailCaptureContainer.style.display = "none";
+    let X = document.querySelector(".bx__btn-close__icon");
+    X.style.display = "none";
+    bxThankYouContainer.style.display = "flex";
+    // flash thank you message
+    setTimeout(function(){ document.querySelector('#bx-transition-card').style.display= 'none'; }, 1500);
 }
+
+const validateEmail = (bxEmail) =>{
+    let validation = false;
+    let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(bxEmail);
+    mailformat ? validation = true: validation = false;
+    return validation;
+}
+
+
