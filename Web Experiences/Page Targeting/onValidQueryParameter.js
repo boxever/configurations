2f9b168441dc07d7e65f1f843ed8850336848f30
bx-conor-oshea <75018305+bx-conor-oshea@users.noreply.github.com>
2021-05-21 12:12:53 +0100

    Added example url including relevant query param

--- Web Experiences/Page Targeting/onValidQueryParameter.js
+++ Web Experiences/Page Targeting/onValidQueryParameter.js
@@ -1,4 +1,5 @@
 // Triggers the experience when only when a query parameter with a specific value is present
+// E.g. www.example.com?bx-qa=1 will trigger the experience with the below configuration
 (function () {
     const targetQueryParameter = 'bx-qa'; // edit here to change the targeted query parameter
     const targetValue = '1'; // edit here to change the value required for the targeted query parameter
