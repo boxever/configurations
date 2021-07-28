ec1695fa680080525408ee9c4e488d2617a586c7
Samuel270 <samuel.pezzaioli@sitecore.com>
2021-07-28 15:44:08 +0200

    sitecore-corner-popup

--- Web Experiences/Blueprints/corner-popup/corner-popup.js
+++ Web Experiences/Blueprints/corner-popup/corner-popup.js
@@ -1,31 +1,55 @@
-insertHTMLAfter("body");
-var bxContent = document.querySelector("#bx-"+variant.ref+ " #bx-transition-card");
+// Adds a unique variant identifier to CSS when deployed to ensure CSS does not impact styling of other elements.
+var compiledCSS = Boxever.templating.compile(variant.assets.css)(variant);
+var styleTag = document.getElementById('style-' + variant.ref)
+if (styleTag) {
+    styleTag.innerHTML = compiledCSS;
+}
+// End Adds a unique variant identifier to CSS when deployed to ensure CSS does not impact styling of other elements.
 
+insertHTMLAfter("body");
+
+// show experience on load
+let scContent = document.querySelector("#sc-"+variant.ref+ " #sc-transition-card");
 setTimeout(function() {
-    bxContent.classList.add("open");
-    sendInteractionToBoxever("VIEWED");
+    scContent.classList.add("open");
+    sendInteractionToSitecoreCDP("VIEWED");
 });
 
-var bxCardClose = document.body.querySelector("#bx-"+variant.ref+ " #bx-transition-card--secondary");
-bxCardClose.onclick = function() {
-    sendInteractionToBoxever("DISMISSED");
-    bxContent.classList.remove("open");
+// declarations
+var scCardClose = document.body.querySelector("#sc-"+variant.ref+ " #CTA-dismiss");
+var scCloseBtn = document.body.querySelector("#sc-"+variant.ref+ " .sc-btn-close");
+var CTAbtn = scContent.querySelector("#CTA-accept");
+
+// close experience function declaration
+const dismissExperience = function(){
+    sendInteractionToSitecoreCDP("DISMISSED");
+    scContent.classList.remove("open");
 }
 
-var bxCardCta = bxContent.querySelector("#bx-transition-card--primary");
-bxCardCta.onclick = function() {
-    sendInteractionToBoxever("CLICKED");
-    window.location.href = "[[CTA destination URL | string || {required:true}]]";
+
+// Listeners
+// dismiss experience on secondary button click
+scCardClose.onclick = function() {
+    dismissExperience();
+}
+// dismiss on X btn click
+scCloseBtn.onclick = function() {
+    dismissExperience();
 }
 
-function sendInteractionToBoxever(interactionType) {
-    var eventToSent = {
+// on Call to action click redirect to specific URL
+CTAbtn.onclick = function() {
+    sendInteractionToSitecoreCDP("CLICKED");
+    window.location.href = "[[CTA destination URL | string || {required:true, group: Button Configuration}]]";
+}
+
+// SC function declaration
+const sendInteractionToSitecoreCDP = function(interactionType) {
+    let eventToSent = {
         "channel": "WEB",
-        "type": "INTERACTION",
+        "type": "[[ Experience ID | String | CORNER | {required: true}]]_" + interactionType,
         "pos": window._boxever_settings.pointOfSale,
         "browser_id": Boxever.getID(),
-        "interactionID":"BX_CORNER_POPUP",
-        "interactionType": interactionType
     };
     Boxever.eventCreate(eventToSent, function (data) { }, 'json');
 }
